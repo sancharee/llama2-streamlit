7c0565b72503d75c222fcd82f2936a2da9877363
sancharee <sanchareedas@gmail.com>
2023-07-27 12:09:47 +0530

    added files

--- src/llm.py
+++ src/llm.py
@@ -20,6 +20,7 @@ def build_llm():
     # Local CTransformers model
     llm = CTransformers(model=cfg.MODEL_BIN_PATH,
                         model_type=cfg.MODEL_TYPE,
+                        model_file=cfg.MODEL_BIN_PATH,
                         config={'max_new_tokens': cfg.MAX_NEW_TOKENS,
                                 'temperature': cfg.TEMPERATURE}
                         )
